ee1a9c7ec42362e570b87a9af7dcf1f307775059
AlexGibby2000 <42209825+AlexGibby2000@users.noreply.github.com>
2025-03-06 20:30:28 -0500

    solved questions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revstr="";
+    for(let i=string.length-1;i>=0;i--){
+        revstr+=string.charAt(i);
+    }
+    return revstr;
 };
 
 // Do not edit below this line
