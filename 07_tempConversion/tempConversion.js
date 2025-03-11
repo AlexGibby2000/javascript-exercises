3bfd3e45d48a3f79caf8a123d0689f23559a0c35
AlexGibby2000 <42209825+AlexGibby2000@users.noreply.github.com>
2025-03-11 15:48:15 -0400

    solved activity 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const fraction=5/9;
+  return Math.round((((temp-32)*fraction)+Number.EPSILON)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fraction=9/5;
+  return Math.round((((temp*fraction)+32)+Number.EPSILON)*10)/10;
 };
 
 // Do not edit below this line
