00d5973f9ee52a224c92d806d6dc30d71214d3da
AlexGibby2000 <42209825+AlexGibby2000@users.noreply.github.com>
2025-03-11 15:15:50 -0400

    solved question 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result=0;
+    if (typeof start!='number'||typeof end !='number'||start<0||end<0||start%1!=0||end%1!=0){
+        return 'ERROR';
+    }
+    if(start>end){
+        let temp=start;
+        start=end;
+        end=temp;
+    }
+    for(let index=start;index<=end;index++){
+        result+=index;
+    }
+    return result;
 };
 
 // Do not edit below this line
